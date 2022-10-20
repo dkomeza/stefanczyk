8a94d55d2cf5ae611d572e9cc005ea95d91fe2d5
Dawid Komęza <dawid.komeza@gmail.com>
2022-10-20 07:00:32 +0000

    ex1 updates

--- kli/webGL-1/static/pages/js/index01.js
+++ kli/webGL-1/static/pages/js/index01.js
@@ -1,1 +1,17 @@
-console.log("supcio")
+window.addEventListener("load", () => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera({fov: 45, aspect: window.innerWidth/window.innerHeight});
+    const renderer = new THREE.WebGLRenderer();
+    const axes = new THREE.AxesHelper(1000)
+    // renderer.setClearColor(0x0066ff);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.position.set(100, 100, 100);
+    camera.lookAt(scene.position);
+    scene.add(axes)
+    document.getElementById("root").appendChild(renderer.domElement)
+    function render() {
+        requestAnimationFrame(render);
+        renderer.render(scene, camera);
+    }
+    render();
+})
