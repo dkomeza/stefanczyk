43c4ee1d3dc7e43b6fed7e302db9253f46857cf7
Dawid Komęza <dawid.komeza@gmail.com>
2022-10-27 05:58:19 +0000

    Readdir fix

--- kli/webGL-2/index.js
+++ kli/webGL-2/index.js
@@ -1,21 +1,28 @@
-const express = require("express")
-const fs = require("fs")
-const app = express()
-const PORT = 3000
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const PORT = 3000;
 
-app.use(express.static('static'))
-app.use(express.static('static/pages')) 
+app.use(express.static("static"));
+app.use(express.static("static/pages"));
 
 app.get("/pages", function (req, res) {
-    fs.readdir(`${__dirname}/static/pages`,function (err, files) {
-        if (err) {
-            return console.log(err);
-        }
-        res.setHeader('content-type', 'application/json');
-        res.send(files)
-    });
-})
+  fs.readdir(
+    `${__dirname}/static/pages`,
+    { withFileTypes: true },
+    function (err, files) {
+      if (err) {
+        return console.log(err);
+      }
+      res.setHeader("content-type", "application/json");
+      const filesNames = files
+        .filter((dirent) => dirent.isFile())
+        .map((dirent) => dirent.name);
+      res.send(filesNames);
+    }
+  );
+});
 
 app.listen(PORT, () => {
-    console.log("Start serwera")
-})
+  console.log("Start serwera");
+});
