13b93aeab5d814e138a8ff66f296a203c4de0fe9
Dawid Komęza <dawid.komeza@gmail.com>
2022-11-09 17:42:55 +0000

    Project update

---
+++ kli/webGL-projekt/app/src/index.ts
@@ -0,0 +1,5 @@
+import { Physics } from "./Physics.js";
+
+let TankPhysics = new Physics()
+
+console.log(TankPhysics)
--- kli/webGL-projekt/index.js
+++ kli/webGL-projekt/index.js
@@ -1,65 +1,17 @@
-import Stats from "three/addons/libs/stats.module.js";
+const express = require("express");
+const app = express();
+const PORT = 3000;
 
-// import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-// import { RoomEnvironment } from "three/addons/environments/RoomEnvironment.js";
+const data = require("./app/data/tanks.json");
 
-// import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-// import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
+app.use(express.static("app/"));
 
-window.addEventListener("load", function () {
-  createScene();
+app.get("/data", function (req, res) {
+  if (data[req.query.tank]) {
+    res.send(JSON.stringify(data[req.query.tank]));
+  }
 });
 
-const scene = new THREE.Scene();
-const axes = new THREE.AxesHelper(1000);
-const renderer = new THREE.WebGLRenderer();
-const camera = new THREE.PerspectiveCamera(
-  45,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  10000
-);
-
-function createScene() {
-  scene.add(axes);
-  renderer.setClearColor(0x0066ff);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.getElementById("root").appendChild(renderer.domElement);
-  camera.position.set(200, 200, 200);
-  camera.lookAt(scene.position);
-  let car = loadCar();
-  scene.add(car);
-  render();
-}
-
-function loadCar() {
-  const dracoLoader = new DRACOLoader();
-  dracoLoader.setDecoderPath("js/libs/draco/gltf/");
-
-  const loader = new GLTFLoader();
-  loader.setDRACOLoader(dracoLoader);
-  loader.load(
-    "models/gltf/LittlestTokyo.glb",
-    function (gltf) {
-      const model = gltf.scene;
-      model.position.set(1, 1, 0);
-      model.scale.set(0.01, 0.01, 0.01);
-      scene.add(model);
-
-      mixer = new THREE.AnimationMixer(model);
-      mixer.clipAction(gltf.animations[0]).play();
-
-      animate();
-    },
-    undefined,
-    function (e) {
-      console.error(e);
-    }
-  );
-}
-
-function render() {
-  requestAnimationFrame(render);
-  camera.lookAt(scene.position);
-  renderer.render(scene, camera);
-}
+app.listen(PORT, function () {
+  console.log("Supcio");
+});
